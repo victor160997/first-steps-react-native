fefc8c009288e15744d46eee9154304a1d08dbc3
Victor Mendonca <victor.men.dev@gmail.com>
2021-11-11 09:24:52 -0300

    primeiros pasos react native

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet, Image, Button } from 'react-native';
+import styles, { Text20, Body, DogImage } from './styles/styles';
+import Padrao from './styles/styles';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Body>
+      <Text20>App!</Text20>
+      <DogImage source={ { uri: 'https://www.doglife.com.br/site/assets/images/cao.png' } } style={ [Padrao.shadow] }/>
+      <Button
+        title="Learn More"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
+    </Body>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles/styles.js
@@ -0,0 +1,52 @@
+import { StyleSheet } from "react-native";
+import styled from "styled-components/native"
+
+export const Body = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+`;
+
+export const Text20 = styled.Text`
+  font-size: 70px;
+  text-shadow: 0px 5px 15px black;
+`;
+
+
+export const DogImage = styled.Image`
+  margin-top: 50px;
+  width: 200px;
+  height: 200px;
+  border-radius: 360px;
+  /* border: solid black 3px; */
+  box-shadow: 5px 5px 15px white;
+`;
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'red',
+  },
+  f20: {
+    fontSize: 70,
+    borderWidth: 2,
+    borderRadius: 10,
+    borderColor: 'blue',
+    fontWeight: 'bold',
+    padding: 15,
+  },
+  shadow: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 12,
+    },
+    shadowOpacity: 0.58,
+    shadowRadius: 16.00,
+
+    elevation: 24,
+  }
+});
